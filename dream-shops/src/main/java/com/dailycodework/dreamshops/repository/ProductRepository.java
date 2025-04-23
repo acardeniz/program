804597dddd89114bfa83e8265ace8540b9ca6370
acardeniz <denizacar19731973@hotmail.com>
2025-04-23 21:29:21 +0300

    ilk yükleme

---
+++ dream-shops/src/main/java/com/dailycodework/dreamshops/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.dailycodework.dreamshops.repository;
+
+import com.dailycodework.dreamshops.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryName(String categoryName);
+    List<Product> findByBrand(String brand);
+    List<Product> findByCategoryNameAndBrand(String categoryName, String brand);
+    List<Product> findByName(String name);
+    List<Product> findByBrandAndName(String brand, String name);
+    Long countByBrandAndName(String brand, String name);
+
+    List<Product> findByCategoryName(String brand, String name);
+}
